d72d76603bfaf000e2db63821f09ed8f22519a3c
Blaine Pace <blainepace@gmail.com>
2013-04-05 21:08:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocketdrop::Application.config.secret_token = '50e00f222d843cc8b622d305822870ac06dfcc14cbc7a917f2afe7c48c3c375b90cd8b8260ac8f17554d9ebd54f1a1fafc492cf3366c51afe7f3e07e369ca4ae'
